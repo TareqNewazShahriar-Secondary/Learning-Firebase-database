fa5980da1e32bb51a37b0c62888e639a191fb013
TareqNewazShahriar <tareq.newaz.shahriar@gmail.com>
2018-08-01 00:17:40 +0600

    hhh

--- TravelMantics/app/src/main/java/com/doesnothaveadomain/travelmantics/FirebaseUtil.java
+++ TravelMantics/app/src/main/java/com/doesnothaveadomain/travelmantics/FirebaseUtil.java
@@ -1,5 +1,9 @@
 package com.doesnothaveadomain.travelmantics;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.common.stats.StatisticalEventTrackerProvider;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -12,6 +16,8 @@ public class FirebaseUtil
 	public static FirebaseDatabase mFirebaseDb;
 	public static DatabaseReference mDbRef;
 	private static FirebaseUtil mFirebaseUtil;
+	private static FirebaseAuth mFirebaseAuth;
+	private static FirebaseAuth.AuthStateListener mAuthStateListner;
 	public static ArrayList<TravelDeal> mTravelDeals;
 	
 	private FirebaseUtil() { }
@@ -22,6 +28,15 @@ public class FirebaseUtil
 		{
 			mFirebaseUtil = new FirebaseUtil();
 			mFirebaseDb = FirebaseDatabase.getInstance();
+			mFirebaseAuth = FirebaseAuth.getInstance();
+			mAuthStateListner = new FirebaseAuth.AuthStateListener()
+			{
+				@Override
+				public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+				{
+					
+				}
+			};
 		}
 		mTravelDeals = new ArrayList<>();
 		mDbRef = mFirebaseDb.getReference().child(dbPath);
